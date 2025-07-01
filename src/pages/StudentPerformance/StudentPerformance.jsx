d419ddc395d34c646ffb6247baf5ff7adc26795c
sibikrishnaYCC <sibikrishnakrishna@gmail.com>
2025-07-02 02:15:27 +0530

    commeting

--- src/pages/StudentPerformance/StudentPerformance.jsx
+++ src/pages/StudentPerformance/StudentPerformance.jsx
@@ -28,13 +28,13 @@ export default function StudentPerformance({ theme }) {
       </div>
 
       {/* Bar Chart Skeleton or Chart */}
-      <div className="w-full max-w-2xl">
+      {/* <div className="w-full max-w-2xl">
         {loading ? (
           <div className="w-full h-[320px] bg-gray-200 dark:bg-gray-800 rounded-lg animate-pulse" />
         ) : (
           <SubjectGradeBarChart theme={theme} />
         )}
-      </div>
+      </div> */}
 
       {/* Radar Chart Skeleton or Chart */}
       {/* <div className="w-full max-w-2xl">
