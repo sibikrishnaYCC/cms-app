1ed787d9bc58c6447dd1f49e6dd4f6fd705e784b
sibikrishnaYCC <sibikrishnakrishna@gmail.com>
2025-07-01 14:59:44 +0530

    removing slash

--- base.js
+++ base.js
@@ -1,5 +1,5 @@
 // const baseurl = 'http://192.168.200.18:3000';
 // export default baseurl;
 
-const baseurl = 'https://cms-backend-blush.vercel.app/';
+const baseurl = 'https://cms-backend-blush.vercel.app';
 export default baseurl;
