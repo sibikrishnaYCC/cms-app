b227604f99195f99359a45d8769dd709b3543a37
sibikrishnaYCC <sibikrishnakrishna@gmail.com>
2025-07-02 01:55:59 +0530

    removing jsx on every recent file

--- src/pages/StudentPerformance/SubjectGradeBarChart.jsx
+++ src/pages/StudentPerformance/SubjectGradeBarChart.jsx
@@ -8,7 +8,7 @@ import {
   CardDescription,
   CardContent,
   CardFooter,
-} from "@/components/ui/card.jsx";
+} from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import {
   ChartContainer,
